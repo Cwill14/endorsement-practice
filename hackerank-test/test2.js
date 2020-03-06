30ede355fc641edf70a52cf0b85530133077e0c5
Clark Williams <theclarkwilliams@gmail.com>
2020-03-06 16:33:53 -0700

    test2

--- hackerank-test/test2.js
+++ hackerank-test/test2.js
@@ -1,94 +1,83 @@
-// ANAGRAM
+// Max of array queries
 
-function makeAnagrams(a, b) {
-    // Write your code here
-    // store instances in object
-    let trackerA = {}
-    let trackerB = {}
-    // add number of occurences
-    a.split("").map(char => {
-        trackerA[char] 
-            ? trackerA[char]++
-            : trackerA[char] = 1
-    })
-    // compare occurences ?
-
-
-}
-
-// CONDENSED LIST
-
-function condense(head) {
-    // Write your code here
-    // set current node to head to start
-    let current = head
-    // set previous node to null to start
-    let prev = null
-    // store node values occurrences in object
-    const nodeValues = {}
-    // loop through nodes
-    while (current) {
-        console.log("prev = ", prev)
-        console.log("current = ", current)
-        // console.log("current.data = ", current.data)
-        // console.log("prev = ", prev)
-        // if node value already exists in object
-        if (nodeValues[current.data]) {
-            nodeValues[current.data]++
-            // remove node from list
-            // to do so, need to reassign prev.next to the current.next
-            // if current.next is null, reassign prev.next to null
-            // console.log("prev.next = ", prev.next)
-            // console.log("current.data = ", current.data)
-            // console.log("current.next = ", current.next)
-            // if (current.next === null) {
-            //     console.log("null says hi")
-            //     prev.next = null
-            // } else {
-            //     prev.next = current.next
-            // }
-            prev.next = current.next
-        }
-        // else add to object
-        else {
-            nodeValues[current.data] = 1
-        }
-        // need null condition to fix problem of not deleting last redundant node
-        // console.log("prev = ", prev)
-        // if (prev.next)  
-
-        //  how do i reassign prev.next to current.next and yet still traverse? it works until the end
-        prev = current
-        current = current.next
+function maxArrayQueries(n, queries) {
+    // create array of zeros
+    // let arr = [0] * n
+    let arr = []
+    for (let i = 0; i < n; i++) {
+        arr.push(0)
     }
-    // console.log("nodeValues = ", nodeValues)
-    return head
+    // console.log("arr = ", arr)
 
+    // go through each query at a time
+    queries.forEach(q => {
+        // console.log("q = ", q)
+        console.log("---")
+        let start = q[0]
+        let end = q[1]
+        let k = q[2]
+        // console.log("start = ", start)
+        // console.log("end = ", end)
+        // console.log("k = ", k)
+
+        // loop from index start to index end, for each arr[index], add k
+        for (let i = start; i <= end; i++) {
+            // console.log("i = ", i)
+            // must be -1 because of 1-based array
+            arr[i -1] += k
+        }
+    })
+    // console.log("arr = ", arr)
+
+    // return max value of array
+    return Math.max(...arr)
 }
 
-// REVERSE WITHIN PARENTHESES
+// Note construction
 
-function reverseInParentheses(s) {
+function constructNote(magazine, note) {
     // Write your code here
-    // want to pull out parentheses sections, reverse, and add them back in
 
-    // captures all strings inside parentheses
-    const pGroups = s.match(/\([^()]+\)/gi)
-    // replaces those sections with a placeholder 
-    const replaced = s.replace(/\([^()]+\)/gi, "#")
-    console.log("replaced = ", replaced)
-    // for each group, reverse the string inside parentheses
-    const reversedPGroups = []
-    pGroups.forEach(group => {
-        // adds those to new array once reversed
-        reversedPGroups.push(group.match(/[a-z]/gi).reverse().join(""))
+    let bool = true
+    // go through note, check each word
+    note.forEach(word => {
+        // console.log("note: ", word)
+        // console.log("magazine.includes(word): ", magazine.includes(word))
+        if (!magazine.includes(word)) {
+        //  indexOf returns -1 if element is not in array, otherwise returns index of element
+        // if not in magazine, return false
+        // if (magazine.indexOf(word) === -1) {
+            // return false
+            bool = false
+        } else {
+            // if it is magazine, remove from magazine and continue
+            let index = magazine.indexOf(word)
+            // delete magazine[index]
+            magazine.splice(index, 1)
+        }
+
     })
-    console.log(reversedPGroups)
-    // for each reversed group, replace into replaced string
-    // reversedPGroups.forEach(group => {
-    //     replaced.replace(/#/, group)
-    // })
-    replaced.replace(/\W/, reversedPGroups[0])
-    console.log("replaced after = ", replaced)
-    
+    // return true
+    return bool
+}
+
+// Array partitioning
+function partitionArray(k, numbers) {
+    // Write your code here
+
+    // trying to figure out the right variables for slicing
+    console.log("numbers = ", numbers)
+    let sub1 = numbers.slice(0, numbers.length / k)
+    console.log("sub1 = ", sub1)
+    console.log("numbers = ", numbers)
+    console.log("(numbers.length / k) + 1 = ", (numbers.length / k) +1 )
+    let sub2 = numbers.slice((numbers.length / k), numbers.length)
+    console.log("sub2 = ", sub2)
+    console.log("numbers = ", numbers)
+
+    // slice array based on k, store those somewhere?
+    for (let i = 0; i < k; i++) {
+        // numbers.slice(i)
+    }
+
 }
